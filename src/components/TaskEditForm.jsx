1b6d3bfd80975e42581c9da09fa2b9d030ec6f6d
Rizwan <rkhan@nttglobal.net>
2022-07-14 13:28:39 +0530

    ADD: initial code

---
+++ src/components/TaskEditForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskEditForm() {
+  return (
+    <div>TaskEditForm</div>
+  )
+}
+
+export default TaskEditForm
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './taskList.css'
+
+function TaskList() {
+  return (
+    <div></div>
+  )
+}
+
+export default TaskList
